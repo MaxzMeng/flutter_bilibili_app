5ca75a5c9b8c46c3b24db084a2633c1b4b8610e5
MaxzMeng <maxzmeng@gmail.com>
2021-06-12 20:47:33 +0800

    [Feature] Add route
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

---
+++ lib/page/video_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bilibili_app/model/video_model.dart';
+
+class VideoDetailPage extends StatefulWidget {
+  final VideoModel videoModel;
+
+  const VideoDetailPage(this.videoModel);
+
+  @override
+  _VideoDetailPageState createState() => _VideoDetailPageState();
+}
+
+class _VideoDetailPageState extends State<VideoDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Text('视频详情页，vid:${widget.videoModel.vid}'),
+      ),
+    );
+  }
+}
