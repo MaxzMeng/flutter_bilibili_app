9fb130a5bf6bd710f789584b4c911ccdc0ff34dc
MaxzMeng <maxzmeng@gmail.com>
2021-06-18 00:02:22 +0800

    [Feature] add top navigator
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bilibili_app/model/video_model.dart';
 import 'package:flutter_bilibili_app/navigator/hi_navigator.dart';
+import 'package:flutter_bilibili_app/page/home_tab_page.dart';
+import 'package:flutter_bilibili_app/util/color.dart';
+import 'package:underline_indicator/underline_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,12 +12,16 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
   var _listener;
+  late TabController _tabController;
+  var tabs = ["推荐", "热门", "追播", "影视", "搞笑", "日常", "综合", "手机游戏", "短片·手书·配音"];
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
     HiNavigator.getInstance().addListener(this._listener = (current, pre) {
       print('home:current:${current.page}');
       print('home:pre:${pre.page}');
@@ -36,20 +43,41 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Column(
-          children: [
-            Text('首页'),
-            MaterialButton(
-                child: Text('详情'),
-                onPressed: () => {
-                      HiNavigator.getInstance().onJumpTo(RouteStatus.detail,
-                          args: {'videoMo': VideoModel(1001)})
-                    })
-          ],
-        ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.only(top: 30),
+            child: _tabBar(),
+          ),
+          Flexible(
+              child: TabBarView(
+                  controller: _tabController,
+                  children: tabs.map((e) => HomeTabPage(name: e)).toList()))
+        ],
       ),
     );
+  }
+
+  _tabBar() {
+    return TabBar(
+        controller: _tabController,
+        isScrollable: true,
+        labelColor: Colors.black,
+        indicator: UnderlineIndicator(
+            strokeCap: StrokeCap.round,
+            borderSide: BorderSide(color: primary, width: 3),
+            insets: EdgeInsets.only(left: 15, right: 15)),
+        tabs: tabs.map<Tab>((tab) {
+          return Tab(
+              child: Padding(
+            padding: EdgeInsets.only(left: 5, right: 5),
+            child: Text(
+              tab,
+              style: TextStyle(fontSize: 16),
+            ),
+          ));
+        }).toList());
   }
 
   @override
