7a687e501bb3eaa8c95eb9dbc8ba1558cfdd5e2f
MaxzMeng <maxzmeng@gmail.com>
2021-06-25 00:33:22 +0800

    [Feature] Add video card
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

--- lib/widget/video_card.dart
+++ lib/widget/video_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bilibili_app/model/video_model.dart';
+import 'package:flutter_bilibili_app/navigator/hi_navigator.dart';
+import 'package:flutter_bilibili_app/util/format_util.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class VideoCard extends StatelessWidget {
   final VideoModel videoModel;
@@ -9,8 +12,139 @@ class VideoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
-      child: Image.network(videoModel.cover),
+        onTap: () {
+          HiNavigator.getInstance()
+              .onJumpTo(RouteStatus.detail, args: {"videoMo": videoModel});
+        },
+        child: SizedBox(
+          height: 200,
+          child: Card(
+            margin: EdgeInsets.only(left: 4, right: 4, bottom: 8),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [_itemImage(context), _infoText()],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  _itemImage(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        FadeInImage.memoryNetwork(
+          height: 120,
+          width: size.width / 2 - 20,
+          placeholder: kTransparentImage,
+          image: videoModel.cover,
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+                padding: EdgeInsets.only(left: 8, right: 8, bottom: 3, top: 5),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [Colors.black54, Colors.transparent])),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(right: 2),
+                          child:
+                              _iconText(Icons.ondemand_video, videoModel.view),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 2),
+                          child: _iconText(
+                              Icons.favorite_border, videoModel.favorite),
+                        ),
+                      ],
+                    ),
+                    _iconText(null, videoModel.duration),
+                  ],
+                )))
+      ],
+    );
+  }
+
+  _iconText(IconData? iconData, int count) {
+    String views = "";
+    if (iconData != null) {
+      views = countFormat(count);
+    } else {
+      views = durationTransform(videoModel.duration);
+    }
+    return Row(
+      children: [
+        if (iconData != null) Icon(iconData, color: Colors.white, size: 12),
+        Padding(
+            padding: EdgeInsets.only(left: 3),
+            child: Text(views,
+                style: TextStyle(color: Colors.white, fontSize: 10)))
+      ],
+    );
+  }
+
+  _infoText() {
+    return Expanded(
+        child: Container(
+      padding: EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            videoModel.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 12, color: Colors.black87),
+          ),
+          //作者
+          _owner()
+        ],
+      ),
+    ));
+  }
+
+  _owner() {
+    var owner = videoModel.owner;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  owner.face,
+                  width: 24,
+                  height: 24,
+                )),
+            Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Text(
+                owner.name,
+                style: TextStyle(fontSize: 11, color: Colors.black87),
+              ),
+            )
+          ],
+        ),
+        Icon(
+          Icons.more_vert_sharp,
+          size: 15,
+          color: Colors.grey,
+        )
+      ],
     );
   }
 }
