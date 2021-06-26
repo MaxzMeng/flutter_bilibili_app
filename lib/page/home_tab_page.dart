80df51e13a66a5bdf28ad82e3cd21cc0a52094fe
MaxzMeng <maxzmeng@gmail.com>
2021-06-26 23:48:06 +0800

    [Feature] Add load more
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

--- lib/page/home_tab_page.dart
+++ lib/page/home_tab_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bilibili_app/http/core/hi_error.dart';
 import 'package:flutter_bilibili_app/http/dao/home_dao.dart';
 import 'package:flutter_bilibili_app/model/home_mo.dart';
 import 'package:flutter_bilibili_app/model/video_model.dart';
+import 'package:flutter_bilibili_app/util/color.dart';
 import 'package:flutter_bilibili_app/util/toast_util.dart';
 import 'package:flutter_bilibili_app/widget/hi_banner.dart';
 import 'package:flutter_bilibili_app/widget/video_card.dart';
@@ -19,42 +20,65 @@ class HomeTabPage extends StatefulWidget {
   _HomeTabPageState createState() => _HomeTabPageState();
 }
 
-class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClientMixin{
+class _HomeTabPageState extends State<HomeTabPage>
+    with AutomaticKeepAliveClientMixin {
   List<VideoModel> videoList = [];
   int pageIndex = 1;
+  bool _loading = false;
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    _scrollController.addListener(() {
+      var dis = _scrollController.position.maxScrollExtent -
+          _scrollController.position.pixels;
+      if (dis < 300) {
+        _loadData(loadMore: true);
+      }
+    });
     _loadData();
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      context: context,
-      removeTop: true,
-      child: StaggeredGridView.countBuilder(
-          padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-          itemCount: videoList.length,
-          crossAxisCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (widget.bannerList != null && index == 0) {
-              return Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: _banner(),
-              );
-            } else {
-              return VideoCard(videoModel: videoList[index]);
-            }
-          },
-          staggeredTileBuilder: (int index) {
-            if (widget.bannerList != null && index == 0) {
-              return StaggeredTile.fit(2);
-            } else {
-              return StaggeredTile.fit(1);
-            }
-          }),
+    super.build(context);
+    return RefreshIndicator(
+      child: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: StaggeredGridView.countBuilder(
+            controller: _scrollController,
+            physics: const AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+            itemCount: videoList.length,
+            crossAxisCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              if (widget.bannerList != null && index == 0) {
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: _banner(),
+                );
+              } else {
+                return VideoCard(videoModel: videoList[index]);
+              }
+            },
+            staggeredTileBuilder: (int index) {
+              if (widget.bannerList != null && index == 0) {
+                return StaggeredTile.fit(2);
+              } else {
+                return StaggeredTile.fit(1);
+              }
+            }),
+      ),
+      onRefresh: _loadData,
+      color: primary,
     );
   }
 
@@ -66,6 +90,10 @@ class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClient
   }
 
   Future<void> _loadData({loadMore = false}) async {
+    if(_loading) {
+      return;
+    }
+    _loading = true;
     if (!loadMore) {
       pageIndex = 1;
     }
@@ -91,6 +119,8 @@ class _HomeTabPageState extends State<HomeTabPage> with AutomaticKeepAliveClient
     } on HiNetError catch (e) {
       print(e);
       showWarnToast(e.message);
+    }finally{
+      _loading = false;
     }
   }
 
